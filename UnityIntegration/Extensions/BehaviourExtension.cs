ff857e01cdac2711fd606971a5f22233b76f596b
CheeseCake <grabarzpolski@gmail.com>
2023-05-25 00:35:27 +0200

    added an option to create a link between game object and entity

---
+++ UnityIntegration/Extensions/BehaviourExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Secs;
+using Secs.UnityIntegration;
+using UnityEngine;
+
+namespace Secs.UnityIntegration 
+{
+    public static class BehaviourExtension
+    {
+        public static void Link(this GameObject linkedObject, EcsWorld world, int entityId)
+        {
+            if (!linkedObject.TryGetComponent(out EcsEntityReference reference))
+                reference = linkedObject.AddComponent<EcsEntityReference>();
+            
+            reference.Link( world, entityId);
+        }
+        
+        public static void UnLink(this GameObject linkedObject, EcsWorld world, int entityId)
+        {
+            if (!linkedObject.TryGetComponent(out EcsEntityReference reference))
+                throw new ArgumentException("The object must have Entity Reference before unlinking it");
+            
+            reference.Unlink( world, entityId);
+        }
+    }
+}
---
+++ UnityIntegration/Mono/EcsEntityReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace Secs.UnityIntegration
+{
+    public sealed class EcsEntityReference : MonoBehaviour
+    {
+        private int _entityId = -1;
+        private EcsWorld _world;
+
+        public int EntityId => _entityId;
+        public EcsWorld World => _world;
+
+        public void Link(EcsWorld ecsWorld, int entityId)
+        {
+            if (this._entityId != -1)
+                throw new ArgumentException("This entity reference is occupied by other entity");
+
+            _world = ecsWorld;
+            _entityId = entityId;
+        }
+        
+        public void Unlink(EcsWorld ecsWorld, int entity)
+        {
+            if (this._entityId == -1)
+                throw new ArgumentException("Link entity before unlinking it");
+            
+            _world = null;
+            _entityId = -1;
+        }
+    }
+}

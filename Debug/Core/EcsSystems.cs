6ddf403e1a7bad7242bdcdac64489e7ca4361564
Luke-Wojciechowski <41232205+Luke-Wojciechowski@users.noreply.github.com>
2023-06-06 22:11:45 +0200

    #if Unity editor added

--- Debug/Core/EcsSystems.cs
+++ Debug/Core/EcsSystems.cs
@@ -1,24 +1,30 @@
-﻿using System.Collections.Generic;
+#if UNITY_EDITOR
+using System.Collections.Generic;
 using Secs.Debug;
 using UnityEngine;
-
+#endif
 namespace Secs
 {
     public sealed partial class EcsSystems
     {
+#if UNITY_EDITOR
         internal List<IEcsSystem> AllSystems => _allSystems;
-
+#endif
         public void AttachProfiler()
         {
+#if UNITY_EDITOR
             if (EcsWorldsObserver.Instance == null)
                 new GameObject("Profiler").AddComponent<EcsWorldsObserver>();
             
             EcsWorldsObserver.Instance?.AttachObserver(_world,this);
+#endif    
         }
 
         public void ReleaseProfiler()
         {
+#if UNITY_EDITOR            
             EcsWorldsObserver.Instance?.ReleaseObserver(_world,this);
+#endif            
         }
     }
 }
